ff18baf3eb91415fba7749d800d2bfdf7f5625d4
Fabian Senf <senf@tropos.de>
2018-09-21 06:04:55 +0000

    also cutout

---
+++ nawdex_analysis/config.py
@@ -0,0 +1,7 @@
+
+# some important variables
+SEVIRI_cutout = ((114, 1118), (271, 3047))  # edge based region defintion
+
+# center-and-width-based region defintion 
+# !!!! CARE: must be consistent with SEVIRI_cutout !!!!
+NWCSAF_region = ((617, 1660), (1004, 2776)) 
