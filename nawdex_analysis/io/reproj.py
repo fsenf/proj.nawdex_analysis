73286a976299a8668d1ea95a3f967239d7331e2c
Fabian Senf <senf@tropos.de>
2018-10-01 14:19:17 +0000

    bugfix in reproj: mitigate the nan problem for interpolation in lon/lat

--- nawdex_analysis/io/reproj.py
+++ nawdex_analysis/io/reproj.py
@@ -8,18 +8,157 @@ import os, sys, copy
 import numpy as np
 import scipy.ndimage
 import datetime
+import pyproj
+import warnings
+
 
 import tropy.analysis_tools.grid_and_interpolation as gi
-import tropy.io_tools.netcdf as ncio
 
-from nawdex_analysis.config import  meteosat_georef_file
+from nawdex_analysis.config import SEVIRI_cutout
+
+######################################################################
+# (1) SEVIRI projection and co-ordinate transformations
+######################################################################
+
+
+def msevi_ll2xy(lon, lat, hres = False, lon0 = 0):
+        
+        ''' 
+        Returns line/column numbers in geostationary satellite
+        projection
+        
+        Parameters
+        ----------
+        lon : numpy array
+            longitude
+        
+        lat : numpy array
+            latitude
+            
+        
+        Returns
+        -------
+        x : numpy array
+            x-coordinate in SEVIRI projection
+        
+        y : numpy array
+            y-coordinate in SEVIRI projection
+
+        '''
+        geos_proj_param = {
+                'proj':      'geos',
+                'h':     35785831.0,
+                'a':      6378169.0,
+                'b':      6356583.8,
+                'lon_0':        lon0
+        }
+        # 
+        assert lat.shape==lon.shape
+        
+        # Apply MSG SEVIRI satellite projection
+        msevi_proj = pyproj.Proj(**geos_proj_param)
+        
+        
+        (x, y) = msevi_proj(lon.flatten(),lat.flatten())
+
+        x = x.reshape(*lon.shape)
+        y = y.reshape(*lat.shape)
+        
+        return x, y
 
 
 ######################################################################
-# (1) Nearest Neighbor Interpolation
 ######################################################################
 
-def get_vector2msevi_index( vgeo, msevi_georef_file = None ):
+def msevi_xy2ij(x, y, hres = False):
+        
+        '''
+        Converts msevi xy coordinates into indices.
+        
+        
+        Parameters
+        ----------
+        x : numpy array
+            x-coordinate in SEVIRI projection
+        
+        y : numpy array
+            y-coordinate in SEVIRI projection
+
+
+        Returns
+        -------
+        irow : numpy array
+             row index in SEVIRI projection
+        
+        icol : numpy array
+            column index in SEVIRI projection
+
+        '''
+        
+        # Convert to SEVIRI LRES full-disk lin/col indices
+        if hres == False:
+                res = 3000.40316582
+                icol = x / res + 1856.0
+                irow = 1856.0 - y / res
+        else:
+                res = 1000.1343886066667
+                icol = x / res + (3*1856.0)
+                irow = (3*1856.0) - y / res
+
+        irow.shape = icol.shape = y.shape
+        
+        return irow, icol
+
+######################################################################
+######################################################################
+
+
+def msevi_ij2xy(irow, icol, hres = False):
+        
+        '''
+        Converts msevi indices into xy coordinates.
+        
+        
+        Parameters
+        ----------
+        irow : numpy array
+             row index in SEVIRI projection
+        
+        icol : numpy array
+            column index in SEVIRI projection
+
+
+        Returns
+        -------
+        x : numpy array
+            x-coordinate in SEVIRI projection
+        
+        y : numpy array
+            y-coordinate in SEVIRI projection
+
+        '''
+        
+        # Convert to SEVIRI LRES full-disk lin/col indices
+        if hres == False:
+            res = 3000.40316582
+            offset = 1856.0
+        else:
+            res = 1000.1343886066667
+            offset = (3*1856.0)
+            
+        x = res * (icol - offset)
+        y = res * (offset - irow)
+
+        x.shape = y.shape = irow.shape
+        
+        return x,y
+
+
+######################################################################
+# (2) Nearest Neighbor Interpolation
+######################################################################
+
+def get_vector2msevi_index( vgeo ):
 
     '''
     Calculates nearest neighbor index for the conversion between 
@@ -37,29 +176,33 @@ def get_vector2msevi_index( vgeo, msevi_georef_file = None ):
         index for nn interpolation
     '''
 
+    # prepare target fields ..........................................
+    
+    # get shape
+    (ir1, ir2), (ic1, ic2) = SEVIRI_cutout
+    nrows = ir2 - ir1
+    ncols = ic2 - ic1
+
+    # index set 
+    irow, icol = gi.make_index_set(nrows, ncols)
+
+    # and projection co-ordinates
+    xsevi, ysevi = msevi_ij2xy(irow, icol)
     
 
-    # read georef ----------------------------------------------------
-    if msevi_georef_file is None:
 
-        # hope that meteosat file is there
-        msevi_georef_file = meteosat_georef_file
-    
-    msevi_georef = ncio.read_icon_4d_data( msevi_georef_file, ['lon', 'lat'], itime = None)
-
-
-    # prepare the reprojection ---------------------------------------
+    # prepare input fields ...........................................
     
     # reshape because gi - tools expects 2dim fields
     vlon = vgeo['lon'].reshape(1, -1)
     vlat = vgeo['lat'].reshape(1, -1)
 
-    # tagret grid georef
-    glon = msevi_georef['lon']
-    glat = msevi_georef['lat']
+    xsim, ysim = msevi_ll2xy(vlon, vlat, lon0 = 0)
+    
 
-    # use tool for nn interpolation
-    ind = gi.create_interpolation_index(vlon, vlat, glon, glat)
+    # use tool for nn interpolation ..................................
+    ind = gi.create_interpolation_index(xsim, ysim, xsevi, ysevi, xy = True)
+    
 
     return ind[1] # go back to vector representation
 
