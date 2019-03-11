1e3b336197d51a497ec8b0f3e1e4b4c8c6782ebe
Fabian Senf <senf@tropos.de>
2019-03-11 09:41:40 +0000

    included simlation set collection into selector functionality

--- nawdex_analysis/io/selector.py
+++ nawdex_analysis/io/selector.py
@@ -18,7 +18,9 @@ all simulations and the observation is selected.
 
 
 ######################################################################
+# (1) Check for netcdf files with same variables and times
 ######################################################################
+
 
 def check_if_nc_has_varname( fname, varname ):
 
@@ -187,4 +189,132 @@ def make_filetime_index( varname, tobject,
     return index
 
 ######################################################################
+# (2) Define experiments sets 
 ######################################################################
+
+def set_selector(set_number):
+    
+    '''
+    Selects mis numbers based on (per-defined) experiment set.
+
+
+    Parameters
+    ----------
+    set_number : int
+        numerical ID for experiment set (1 ... 4)
+
+
+    Returns
+    --------
+    fine_set : list
+       list of mis numbers for fine resolution (<= 10 km)
+
+    coarse_set : list
+       list of mis numbers for coarse resolution (>= 20 km)
+    '''
+
+    if set_number == 1:
+        fine_set = [5,6]
+        coarse_set = [3,4]
+        
+    elif set_number == 2:
+        fine_set = [1,2,3,4]
+        coarse_set = [1,2]
+        
+    elif set_number == 3:
+        fine_set = [7,8]
+        coarse_set = [5,6]
+        
+    elif set_number == 4:
+        fine_set = [9,10]
+        coarse_set = [7,8]
+    else:
+        print 'set not defined'
+            
+    return fine_set, coarse_set
+
+######################################################################
+######################################################################
+
+
+def set_initdate( set_number ):
+        
+    '''
+    Selects init date based on (per-defined) experiment set.
+
+
+    Parameters
+    ----------
+    set_number : int
+        numerical ID for experiment set (1 ... 4)
+
+
+    Returns
+    --------
+    initdate : str
+        initialization date of simulation, fmt = '%Y-%m-%d'
+    '''
+
+    if set_number == 1:
+        initdate = '2016-09-20'
+    
+    elif set_number == 2:
+        initdate = '2016-09-22'
+    
+    elif set_number == 3:
+        initdate = '2016-09-29'
+    
+    elif set_number == 4:
+        initdate = '2016-10-04'
+        
+    return initdate
+
+
+######################################################################
+######################################################################
+
+
+
+def gather_simset( set_number ):
+
+    '''
+    Collects a list of experiment names based on a selected set.
+
+
+    Parameters
+    ----------
+    set_number : int
+        numerical ID for experiment set (1 ... 4)
+
+
+    Returns
+    --------
+    simset : list
+        list of experiment names, e.g. ['nawdexnwp-2km-mis-0009', ... ]
+
+    '''
+
+    
+    resolutions = ['2km', '5km', '10km', '20km', '40km', '80km']
+    fine_set, coarse_set = set_selector( set_number )
+    
+    simset = []
+    for res in resolutions:
+    
+        if res in ['2km', '5km', '10km']:
+            mis_list = fine_set
+            
+        elif res in ['20km', '40km', '80km']:
+            mis_list = coarse_set
+        
+        for mis in mis_list:
+            expname = 'nawdexnwp-%s-mis-%s' % (res, str(mis).zfill(4))
+        
+        
+            simset += [ expname, ]
+            
+    return simset
+
+######################################################################
+######################################################################
+
