789fa851c5a60da8bc64a5b16eca954f14b74cf7
Fabian Senf <senf@tropos.de>
2019-04-02 15:21:39 +0000

    started with analysis routines: * binwise averaging and area fractions

---
+++ nawdex_analysis/analysis/averaging.py
@@ -0,0 +1,115 @@
+#!/usr/bin/env python
+
+
+# coding: utf-8
+
+## Binning and averaging
+
+
+######################################################################
+######################################################################
+
+
+import os, sys, copy, glob
+import numpy as np
+
+######################################################################
+######################################################################
+
+
+def area_fractions( v, a, bins ):
+
+    '''
+    Calculates area fractions of variable values in certain intervals.
+
+    
+    Parameters
+    ----------
+    v : numpy array, 2dim
+        variables field
+
+    a : numpy array, 2dim
+        grid box areas
+
+    bins : numpy array, 1dim
+        set of values used for binning
+
+
+    Returns
+    --------
+    afrac : numpy array, 1dim
+        relative area fractions
+    '''
+
+    # calculate total area
+    atot = 1. * a.sum()
+
+    nbins = len(bins)
+    afrac = np.zeros(( nbins - 1, ))
+
+    # loop over bin intervals
+    for i in range( nbins - 1 ):
+
+        # get outer bin values
+        b1 = bins[i]
+        b2 = bins[i + 1]
+    
+        mask = (v >= b1) & (v < b2) 
+        
+        afrac[i] = a[mask].sum() / atot
+
+    return afrac
+
+######################################################################
+######################################################################
+
+
+def area_weighted_binwise_averages( v, a, selector, bins ):
+
+    '''
+    Calculates area fractions of variable values in certain intervals.
+
+    
+    Parameters
+    ----------
+    v : numpy array, 2dim
+        variables field
+
+    a : numpy array, 2dim
+        grid box areas
+
+    bins : numpy array, 1dim
+        set of values used for binning
+
+
+    Returns
+    --------
+    afrac : numpy array, 1dim
+        relative area fractions
+    '''
+
+    # calculate total area
+    atot = 1. * a.sum()
+
+    # variable times area
+    va = v * a
+    
+    # init ave field
+    nbins = len(bins)
+    v_ave = np.zeros(( nbins - 1, ))
+
+    # loop over bin intervals
+    for i in range( nbins - 1 ):
+
+        # get outer bin values
+        b1 = bins[i]
+        b2 = bins[i + 1]
+    
+        mask = (selector >= b1) & (selector < b2) 
+        
+        v_ave[i] = va[mask].sum() / a[mask].sum()
+
+    return np.ma.masked_invalid( v_ave )
+
+######################################################################
+######################################################################
