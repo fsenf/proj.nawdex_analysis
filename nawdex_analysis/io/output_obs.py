59662c530d05075c982a9a7120380613a35dffcb
fsenf <43379813+fsenf@users.noreply.github.com>
2020-02-07 15:10:43 +0100

    Update output_obs.py

--- nawdex_analysis/io/output_obs.py
+++ nawdex_analysis/io/output_obs.py
@@ -19,16 +19,15 @@ import datetime
 
 # a little hack for readthedocs
 try:
-    import tropy.analysis_tools.grid_and_interpolation as gi
     from tropy.standard_config import local_data_path
 except:
     pass
 
 if True:
+    import tropy.analysis_tools.grid_and_interpolation as gi
     from .tools import convert_time
     from .input_obs import read_msevi, read_radiation_flux_tstack
-#except:
-#    pass
+
 
 
 ######################################################################
